7a681e27b511ae3085b14f0008e112700417f0f4
FedBashilov <40949729+FedBashilov@users.noreply.github.com>
2019-08-05 23:50:46 +0300

    rebuild menu component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,33 +12,14 @@ export class AppComponent {
   constructor(private router: Router) {
   }
 
-
-  setCurrent(event: any){
-      let current: any = document.getElementsByClassName("current")[0];
-      current.classList.remove("current");
-      event.currentTarget.classList.add("current");
-  }
-
-  showOrHideMenu(){
-    let menu: any = document.getElementsByClassName("menu")[0];
+  moveContent(){
     let content: any = document.getElementsByClassName("content")[0];
-    let curtain: any = document.getElementsByClassName("curtain")[0];
-    let icon: any = document.getElementsByClassName("icon")[0];
-
-    if(menu.classList.contains("show")){
-      menu.classList.remove("show");
+    if(content.classList.contains("moved")){
       content.classList.remove("moved");
-      curtain.classList.remove("on");
-      curtain.classList.add("off");
-      icon.classList.remove("cross");
-      icon.classList.add("burger");
     } else {
-      menu.classList.add("show");
       content.classList.add("moved");
-      curtain.classList.remove("off");
-      curtain.classList.add("on");
-      icon.classList.remove("burger");
-      icon.classList.add("cross");
     }
   }
+
+
 }
--- src/app/components/moving-menu/moving-menu.component.ts
+++ src/app/components/moving-menu/moving-menu.component.ts
@@ -20,5 +20,36 @@ export class MovingMenuComponent implements OnInit {
       this.onChanged.emit();
   }
 
+  setCurrent(indexOfChosen: number){
+    let current: any = document.getElementsByClassName("current")[0];
+    let chosen: any = document.getElementsByClassName("line_menu_item")[indexOfChosen];
+    current.classList.remove("current");
+    chosen.classList.add("current");
+  }
+
+  showOrHideMenu(){
+    let movingMenu: any = document.getElementsByClassName("moving_menu")[0];
+    let curtain: any = document.getElementsByClassName("curtain")[0];
+    let lineHeader: any = document.getElementsByClassName("line-header")[0];
+    let icon: any = document.getElementsByClassName("icon")[0];
+
+    if(movingMenu.classList.contains("show")){
+      movingMenu.classList.remove("show");
+      lineHeader.classList.remove("moved");
+      curtain.classList.remove("on");
+      curtain.classList.add("off");
+      icon.classList.remove("cross");
+      icon.classList.add("burger");
+    } else {
+      movingMenu.classList.add("show");
+      lineHeader.classList.add("moved");
+      curtain.classList.remove("off");
+      curtain.classList.add("on");
+      icon.classList.remove("burger");
+      icon.classList.add("cross");
+    }
+
+     this.change();
+  }
 
 }
